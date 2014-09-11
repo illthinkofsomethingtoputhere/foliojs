34ce14b2248076f51fe8625757e3c8f05acd9f9a
Caleb V Brewer <calebthebrewer@gmail.com>
2014-09-11 18:19:56 -0500

    removing API call for now

--- client/development/app.controller.js
+++ client/development/app.controller.js
@@ -1,18 +1,11 @@
 angular.module('app')
 	.controller('head', ['$scope', 'config', function($scope, config) {
 
-		config.load()
-			.then(function() {
-				$scope.branding = config.branding();
-			});
-
+		$scope.branding = config.branding();
 	}])
 	.controller('navigation', ['$scope', 'config', function($scope, config) {
 
-		config.load()
-			.then(function() {
-				$scope.branding = config.branding();
-				$scope.tools = config.tools();
-			});
+		$scope.branding = config.branding();
+		$scope.tools = config.tools();
 
 	}]);
--- client/development/config.service.js
+++ client/development/config.service.js
@@ -1,27 +1,6 @@
 angular.module('app')
 	.service('config', ['$http', '$q', 'defaultConfig', function($http, $q, defaultConfig) {
-		var config = {}	,
-			api = 'api/1/config/',
-			deferred;
-
-		this.load = function() {
-
-			if (deferred) return deferred.promise;
-
-			deferred = $q.defer();
-
-			$http.get(api)
-				.success(function(data) {
-					config = angular.extend(defaultConfig, data);
-					deferred.resolve(config);
-				})
-				.error(function() {
-					config = defaultConfig;
-					deferred.resolve(config);
-				});
-
-			return deferred.promise;
-		}
+		var config = defaultConfig;
 
 		this.branding = function() {
 			return config.branding;
